7f2f776bb7262413ba258f3952d2c86f0a640b1f
Max Huzmiev <max@huzm.ru>
2022-08-05 19:56:31 +0300

    Tried to get symbold from string

--- LogString.cpp
+++ LogString.cpp
@@ -2,8 +2,26 @@
 // Дата: 05 авг 2022
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
+
+using std::string;
+using std::cout;
+using std::endl;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string s = "My string";
+    int i = 0;
+    while (true) {
+        try { cout << s.at(i); }
+        catch (std::out_of_range const& e) {
+            cout << endl << e.what() << endl;
+            break;
+        }
+        i++;
+    }
+
+    
 }
